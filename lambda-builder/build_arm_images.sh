ee448a69baa1c4b24691997a237ef457e5adfc38
Subin <39542978+subeans@users.noreply.github.com>
2022-08-09 11:44:50 +0900

    add if else for serving and convert

--- lambda-builder/build_arm_images.sh
+++ lambda-builder/build_arm_images.sh
@@ -1,50 +1,55 @@
 #!/bin/bash
 
-export IMAGE_NAME="serving"
-export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+serving_build=$1
+convert_build=$2
 
-sudo chmod 666 /var/run/docker.sock
-sudo service docker start
+if $serving_build:
+  export IMAGE_NAME="serving"
+  export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-serving="arm_onnx arm_torch arm_tvm"
+  sudo chmod 666 /var/run/docker.sock
+  sudo service docker start
 
-for serv in $serving; do
-  docker build -f "../lambda-serving/$serv/Dockerfile" -t $IMAGE_NAME'_'$serv . --no-cache
-  docker tag $IMAGE_NAME'_'$serv $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
-  aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
-  aws ecr delete-repository \
-    --repository-name $IMAGE_NAME'_'$serv \
-    --force
+  serving="arm_onnx arm_torch arm_tvm"
 
-  sleep 5
+  for serv in $serving; do
+    docker build -f "../lambda-serving/$serv/Dockerfile" -t $IMAGE_NAME'_'$serv . --no-cache
+    docker tag $IMAGE_NAME'_'$serv $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
+    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
+#     aws ecr delete-repository \
+#       --repository-name $IMAGE_NAME'_'$serv \
+#       --force
 
-  aws ecr create-repository \
-    --repository-name $IMAGE_NAME'_'$serv \
-    --image-scanning-configuration scanOnPush=true \
-    --region us-west-2
+    sleep 5
 
-  sleep 5
+    aws ecr create-repository \
+      --repository-name $IMAGE_NAME'_'$serv \
+      --image-scanning-configuration scanOnPush=true \
+      --region us-west-2
 
-  docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
-done
+    sleep 5
 
-export IMAGE_NAME="convert_torch"
+    docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
+  done
 
-converting="arm_tvm"
+if $convert_build:
+  export IMAGE_NAME="convert_torch"
 
-for serv in $converting; do
-  docker build -f "../lambda-optimize/$serv/Dockerfile" -t $IMAGE_NAME'_'$serv . --no-cache
-  docker tag $IMAGE_NAME'_'$serv $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
-  aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
-  aws ecr delete-repository \
-  --repository-name $IMAGE_NAME'_'$serv \
-  --force
+  converting="arm_tvm"
 
-  sleep 5
+  for serv in $converting; do
+    docker build -f "../lambda-optimize/$serv/Dockerfile" -t $IMAGE_NAME'_'$serv . --no-cache
+    docker tag $IMAGE_NAME'_'$serv $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
+    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
+#     aws ecr delete-repository \
+#     --repository-name $IMAGE_NAME'_'$serv \
+#     --force
 
-  aws ecr create-repository \
-    --repository-name $IMAGE_NAME'_'$serv \
-    --image-scanning-configuration scanOnPush=true \
-    --region us-west-2
-  docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
-done
+    sleep 5
+
+    aws ecr create-repository \
+      --repository-name $IMAGE_NAME'_'$serv \
+      --image-scanning-configuration scanOnPush=true \
+      --region us-west-2
+    docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME'_'$serv
+  done

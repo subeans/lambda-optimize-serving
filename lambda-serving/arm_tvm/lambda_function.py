5ffffe75b6309b2a287e8e8de29d6ddd50a5fbcd
Subin <39542978+subeans@users.noreply.github.com>
2022-08-09 11:33:56 +0900

    running time in except

--- lambda-serving/arm_tvm/lambda_function.py
+++ lambda-serving/arm_tvm/lambda_function.py
@@ -84,15 +84,14 @@ def lambda_handler(event, context):
     convert_time = event['convert_time']
     request_id = context.aws_request_id
     log_group_name = context.log_group_name
-    
-    running_time=0
-   
+      
     if "tvm" in optimizer:
         try:
             start_time = time.time()
             res = tvm_serving(workload_type, framework, model_name, model_size, batchsize)
             running_time = time.time() - start_time
         except:
+            running_time=0
             print(f"Error in {model_name} {model_size} {lambda_memory} {batchsize}") 
 
         return {

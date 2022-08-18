2fa358e261a5fe917133d869c38d98fc14ceb1ef
Subin <39542978+subeans@users.noreply.github.com>
2022-08-18 17:21:29 +0900

    tvm arm path

--- lambda-optimize/arm_tvm/lambda_function.py
+++ lambda-optimize/arm_tvm/lambda_function.py
@@ -29,7 +29,7 @@ def update_results(model_name,model_size,batchsize,lambda_memory,convert_time):
     with open(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_convert.json','w') as f:
         json.dump(info, f, ensure_ascii=False, indent=4)  
     
-    s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_convert.json',BUCKET_NAME,f'results/{optimizer}/{hardware}/{model_name}_{model_size}_{batchsize}_{lambda_memory}_convert.json')
+    s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_convert.json',BUCKET_NAME,f'results/tvm/arm/{model_name}_{model_size}_{batchsize}_{lambda_memory}_convert.json')
     print("upload done : convert time results")
 
 

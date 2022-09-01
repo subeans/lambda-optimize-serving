f377fd88531f35aed1a8630ea47c87b2037b96c8
Subin <39542978+subeans@users.noreply.github.com>
2022-09-01 13:26:38 +0900

    update inference file path

--- lambda-serving/intel_tvm/lambda_function.py
+++ lambda-serving/intel_tvm/lambda_function.py
@@ -37,10 +37,10 @@ def update_results(framework,model_name,model_size,batchsize,lambda_memory,infer
         json.dump(info, f, ensure_ascii=False, indent=4)  
     
     if "onnx" in framework :
-        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json',BUCKET_NAME,f'results/tvm/intel/onnx/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json')
+        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json',BUCKET_NAME,f'results/tvm/intel/onnx/inference/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json')
         print("upload done : convert time results")
     else:
-        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json',BUCKET_NAME,f'results/tvm/intel/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json')
+        s3_client.upload_file(f'/tmp/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json',BUCKET_NAME,f'results/tvm/intel/inference/{model_name}_{model_size}_{batchsize}_{lambda_memory}_inference.json')
         print("upload done : convert time results")      
 
 
